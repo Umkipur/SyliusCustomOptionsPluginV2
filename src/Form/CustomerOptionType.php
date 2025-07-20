e47e2bdd72177f0c32fbaad2910bf4083a56a363
Jan Hrabovsky <hrabovskyjan1@gmail.com>
2025-07-20 11:48:00 +0200

    update grid template

--- src/Form/CustomerOptionType.php
+++ src/Form/CustomerOptionType.php
@@ -31,7 +31,6 @@ final class CustomerOptionType extends AbstractResourceType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         // Array keys are the constants and the values are the translations
-        dump('💥 Using Brille24 CustomerOptionType!');
         $possibleTypes = CustomerOptionTypeEnum::getTranslateArray();
 
         $builder
