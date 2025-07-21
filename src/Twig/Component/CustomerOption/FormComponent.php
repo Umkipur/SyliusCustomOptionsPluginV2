b3290e356d2395014dcde96431902bfee65af949
Jan Hrabovsky <hrabovskyjan1@gmail.com>
2025-07-21 11:10:57 +0200

    update customer option form

--- src/Twig/Component/CustomerOption/FormComponent.php
+++ src/Twig/Component/CustomerOption/FormComponent.php
@@ -11,7 +11,6 @@ use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveArg;
 
-#[AsLiveComponent]
 class FormComponent {
     use LiveCollectionTrait;
 
