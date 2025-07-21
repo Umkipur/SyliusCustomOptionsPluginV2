000dc1d2d311e06a968519ee0885fc71bc73623e
Jan Hrabovsky <hrabovskyjan1@gmail.com>
2025-07-21 11:17:09 +0200

    update customer option form

--- src/Twig/Component/CustomerOption/FormComponent.php
+++ src/Twig/Component/CustomerOption/FormComponent.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace Brille24\SyliusCustomerOptionsPlugin\Twig\Component\CustomerOption;
 
+use Brille24\SyliusCustomerOptionsPlugin\Entity\CustomerOptions\CustomerOptionInterface;
 use Sylius\Bundle\UiBundle\Twig\Component\LiveCollectionTrait;
 use Sylius\Bundle\UiBundle\Twig\Component\ResourceFormComponentTrait;
 use Sylius\Bundle\UiBundle\Twig\Component\TemplatePropTrait;
-use Sylius\Component\Product\Model\ProductOptionInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveArg;
@@ -14,7 +14,7 @@ use Symfony\UX\LiveComponent\Attribute\LiveArg;
 class FormComponent {
     use LiveCollectionTrait;
 
-
+    /** @use ResourceFormComponentTrait<CustomerOptionInterface> */
     use ResourceFormComponentTrait {
         initialize as public __construct;
     }
