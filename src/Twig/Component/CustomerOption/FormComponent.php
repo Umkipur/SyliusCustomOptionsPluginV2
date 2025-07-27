cf69864fc532b5ced29c94c276d862703a143452
Jan Hrabovsky <hrabovskyjan1@gmail.com>
2025-07-27 11:42:53 +0200

    feat(customVariants) - v2.1

--- src/Twig/Component/CustomerOption/FormComponent.php
+++ src/Twig/Component/CustomerOption/FormComponent.php
@@ -14,6 +14,7 @@ use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
 use Symfony\Component\Form\FormFactoryInterface;
+use Sylius\Component\Core\Repository\ChannelRepositoryInterface;
 class FormComponent {
 
     use LiveCollectionTrait;
